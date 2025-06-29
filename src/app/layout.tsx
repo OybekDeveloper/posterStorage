cbc4d77f65177ef76bd3dee0180e01fb00730a75
OybekDeveloper <Programmeroybek211@gmail.com>
2025-06-30 02:29:37 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import HideSpinnerOnLoad from "./components/HideSpinnerOnLoad";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HideSpinnerOnLoad />
+        {children}
+      </body>
     </html>
   );
 }
