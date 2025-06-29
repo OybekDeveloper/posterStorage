5d75ec9118403a5b158faec2262258b32cd7cf92
OybekDeveloper <Programmeroybek211@gmail.com>
2025-06-30 03:24:51 +0500

    first commit

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -49,8 +49,6 @@ export async function GET(req: NextRequest) {
 
     // Tokenni cookie-ga yozamiz
     res.cookies.set("authToken", data.access_token, {
-      httpOnly: false,
-      secure: process.env.NODE_ENV === "production",
       expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // 10 kun
       path: "/",
     });
