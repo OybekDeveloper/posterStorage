f49db2cc9b0fc5c754349981c8ded4de8c973f4d
OybekDeveloper <Programmeroybek211@gmail.com>
2025-06-30 23:54:49 +0500

    dfa

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -2,8 +2,8 @@ import TableExportRow from "../components/TableExportRow";
 
 export default async function page({ params }: { params: { code: string } }) {
   const code = params?.code || "";
-  // const token = await fetch(`http://localhost:3000/api/token?code=${code}`);
-  // const tokenData = await token.json();
+  const token = await fetch(`http://localhost:3000/api/token?code=${code}`);
+  const tokenData = await token.json();
 
   return (
     <section className="container flex flex-col p-12 min-h-screen text-base">
