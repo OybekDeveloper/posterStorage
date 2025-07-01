0def08f2b5c37a264a95bca10a49c780a6cd97bb
OybekDeveloper <Programmeroybek211@gmail.com>
2025-07-01 10:33:04 +0500

    fix

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -2,7 +2,7 @@ import TableExportRow from "../components/TableExportRow";
 
 export default async function page({ params }: { params: { code: string } }) {
   const code = params?.code || "";
-  const token = await fetch(`http://localhost:3000/api/token?code=${code}`);
+  const token = await fetch(`https://poster-storage.vercel.app/api/token?code=${code}`);
   const tokenData = await token.json();
 
   return (
